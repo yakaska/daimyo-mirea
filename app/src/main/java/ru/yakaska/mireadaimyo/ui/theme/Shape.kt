fd228dd67f99d4f69263d5a870298ca9f8801adf
Yakaska <valentin7petr@gmail.com>
2022-11-11 23:10:40 +0300

    Changed shape round dp

--- app/src/main/java/ru/yakaska/mireadaimyo/ui/theme/Shape.kt
+++ app/src/main/java/ru/yakaska/mireadaimyo/ui/theme/Shape.kt
@@ -5,7 +5,7 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val mireaDaimyoShapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(8.dp),
-    large = RoundedCornerShape(12.dp)
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(12.dp),
+    large = RoundedCornerShape(16.dp)
 )
